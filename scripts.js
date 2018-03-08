f200292de844890d99b42a373007f913dfaac7d3
Marcin Ślęzak <zstio.informatyka@gmail.com>
2018-03-08 20:58:13 +0100

    Add files

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Podaj wartość podstawy trójkata', a);
+var h = prompt('Podaj wartość wysokosci trójkata', h);
+var triangleArea = a*h/2;
+
+console.log("Pole trójkata o podstawie a: " + a + " i wysokosci h: " + h + 
+            " jest równe: " + triangleArea);
